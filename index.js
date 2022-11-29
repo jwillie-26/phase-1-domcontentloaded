3898933b4285344b87a86c2b3a429f5d527e3e82
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2022-11-29 12:41:17 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+//DOM content event loaded
+
+//Pasted code solutution
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded")
+    let content = document.getElementById('text').textContent = "This is really cool!"
+    return content 
+  });
